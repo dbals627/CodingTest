55d61fe52ee70d3e5a72db6b3ec86e39b34ae23b
유민 <gkjm123@naver.com>
2024-01-23 20:40:39 +0900

    Level.1

---
+++ 프로그래머스/Level.1/5. x만큼 간격이 있는 n개의 숫자.js
@@ -0,0 +1,19 @@
+// function solution(x, n) {
+//   var answer = [];
+
+//   for (let i = 1; i <= n; i++) {
+//     answer.push(i * x);
+//   }
+
+//   return answer;
+// }
+
+function solution(x, n) {
+  console.log(
+    Array(n)
+    // .fill(x)
+    // .map((v, i) => (i + 1) * v)
+  );
+}
+
+solution(2, 5);
