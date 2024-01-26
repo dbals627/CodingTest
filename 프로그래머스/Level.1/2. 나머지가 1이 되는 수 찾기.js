d4dd823ac606e6bce9f1aad79f82b5f6d4a7a964
유민 <gkjm123@naver.com>
2024-01-26 19:59:21 +0900

    Level.1

--- 프로그래머스/Level.1/2. 나머지가 1이 되는 수 찾기.js
+++
@@ -1,9 +0,0 @@
-function solution(n) {
-  for (let x = 1; x < n; x++) {
-    if (n % x === 1) {
-      return x;
-    }
-  }
-}
-
-solution(10);
--- 프로그래머스/Level.1/5. x만큼 간격이 있는 n개의 숫자.js
+++ 프로그래머스/Level.1/5. x만큼 간격이 있는 n개의 숫자.js
@@ -1,19 +1,18 @@
-// function solution(x, n) {
-//   var answer = [];
-
-//   for (let i = 1; i <= n; i++) {
-//     answer.push(i * x);
-//   }
-
-//   return answer;
-// }
-
 function solution(x, n) {
-  console.log(
-    Array(n)
-    // .fill(x)
-    // .map((v, i) => (i + 1) * v)
-  );
+  var answer = [];
+
+  for (let i = 1; i <= n; i++) {
+    answer.push(i * x);
+  }
+
+  return answer;
+}
+
+///// 다른풀이
+function solution(x, n) {
+  return Array(n)
+    .fill(x)
+    .map((v, i) => (i + 1) * v);
 }
 
 solution(2, 5);
