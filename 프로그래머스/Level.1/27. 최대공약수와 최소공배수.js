610a189e3212e1c224c15975eb2800df6d25a065
유민 <gkjm123@naver.com>
2024-02-18 21:41:44 +0900

    Level.1

---
+++ 프로그래머스/Level.1/27. 최대공약수와 최소공배수.js
@@ -0,0 +1,22 @@
+function solution(n, m) {
+  let gcd = 1;
+
+  for (let i = 2; i <= Math.min(n, m); i++) {
+    if (n % i === 0 && m % i === 0) {
+      gcd = i;
+    }
+  }
+
+  let lcm = 1;
+
+  while (true) {
+    if (lcm % n == 0 && lcm % m == 0) {
+      break;
+    }
+    lcm++;
+  }
+
+  return [gcd, lcm];
+}
+
+solution(3, 12);
