5e37d8dba082f9026b7e50135f3182198ae71b8e
유민 <gkjm123@naver.com>
2024-02-26 16:13:48 +0900

    입출력과 사칙연산

---
+++ 백준/입출력과 사칙연산.py
@@ -0,0 +1,46 @@
+# A+B
+A, B = map(int, input().split())
+print(A + B)
+
+# A-B
+A, B = map(int, input().split())
+print(A - B)
+
+# A*B
+A, B = map(int, input().split())
+print(A * B)
+
+# A/B
+A, B = map(int, input().split())
+print(A / B)
+
+# ??!
+print(input() + '??!')
+
+# 1998년생인 내가 태국에서는 2541년생?!
+print(int(input()) - 543)
+
+# 나머지
+A, B, C = map(int, input().split())
+print((A+B) % C)
+print(((A % C) + (B % C)) % C)
+print((A * B) % C)
+print(((A % C) * (B % C)) % C)
+
+# 곱셈
+A = int(input())
+B = input()
+
+print(A * int(B[2]), A * int(B[1]), A * int(B[0]), A * int(B), sep='\n')
+
+# 꼬마 정민
+A, B, C = map(int,(input().split()))
+print(A + B + C)
+
+# 고양이
+# 역슬래쉬는 두번적어줘야 하나로 인식
+print("\\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \\(__)|")
+
