07ff4e7acf2bb85d99ee986f7dc38669030a7067
유민 <gkjm123@naver.com>
2024-03-03 16:05:40 +0900

    반복문

--- 백준/반복문.py
+++ 백준/반복문.py
@@ -42,3 +42,10 @@ print('int')
 
 # 간단하게
 print(int(input())//4*'long ' + 'int')
+
+# A+B - 7
+T = int(input())
+
+for i in range(1, T+1):
+    a, b = map(int, input().split())
+    print('Case #{}:'.format(i), a + b)
