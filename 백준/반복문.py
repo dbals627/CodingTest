56cc03989884a57c2bd7d94fb58e4fc3b237c8a7
유민 <gkjm123@naver.com>
2024-03-04 17:50:18 +0900

    반복문

--- 백준/반복문.py
+++ 백준/반복문.py
@@ -49,3 +49,17 @@ T = int(input())
 for i in range(1, T+1):
     a, b = map(int, input().split())
     print('Case #{}:'.format(i), a + b)
+
+# A+B - 8
+T = int(input())
+
+for i in range(1, T+1):
+    a, b = map(int, input().split())
+    print('Case #{}:'.format(i), a, '+', b, '=', a + b)
+
+# 별 찍기 -1
+n = int(input())
+
+for i in range(n):
+    star = '*'
+    print('*' * (i+1))
